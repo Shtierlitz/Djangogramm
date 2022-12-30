cb2ac5ed3faaf8c881d00cdd91a4d04056146864
Davydovsyy Ihor <Rollbar1990@gmail.com>
2022-12-30 14:06:21 +0200

    temp

--- djangogramm/djangogramm/prod_settings.py
+++ djangogramm/djangogramm/prod_settings.py
@@ -19,7 +19,9 @@ DATABASES = {
     }
 }
 
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [STATIC_DIR]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# STATIC_DIR = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [STATIC_DIR]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
--- djangogramm/djangogramm/settings.py
+++ djangogramm/djangogramm/settings.py
@@ -80,6 +80,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', #add whitenoise
 ]
 
 ROOT_URLCONF = 'djangogramm.urls'
